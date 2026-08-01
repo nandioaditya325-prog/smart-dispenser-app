a2cffb16873282f164aa925c2b6b33d34a405591
nandioaditya325-prog <nandioaditya325@gmail.com>
2026-08-02 00:41:27 +0700

    fix: handle get request on checkout route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Web\DashboardController;
 
 Route::get('/', [DashboardController::class, 'index']);
+
+// Penanganan jika halaman /checkout diakses via GET (diketik langsung / di-refresh di HP)
+Route::get('/checkout', function () {
+    return redirect('/');
+});
+
 Route::post('/checkout', [DashboardController::class, 'checkout'])->name('checkout');
 Route::get('/payment/{id}', [DashboardController::class, 'payment'])->name('payment');
 Route::post('/payment-success/{id}', [DashboardController::class, 'paySuccess'])->name('pay.success');
